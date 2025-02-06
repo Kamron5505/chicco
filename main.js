9b1e751e7b561158f595edab9b9faa386d31ea60
Kamron5505 <kama03346@gmail.com>
2025-02-06 16:36:56 +0500

    aaaa

---
+++ main.js
@@ -0,0 +1,12 @@
+let firstName = prompt("Введите ваше имя:");
+let lastName = prompt("Введите вашу фамилию:");
+let age = prompt("Введите ваш возраст:");
+let city = prompt("Где вы живете?");
+let favoriteSubject = prompt("Какой ваш любимый предмет?");
+alert("Спасибо за Внимание");
+
+console.log("Имя: " + firstName);
+console.log("Фамилия: " + lastName);
+console.log("Возраст: " + age);
+console.log("Город: " + city);
+console.log("Любимый предмет: " + favoriteSubject);
